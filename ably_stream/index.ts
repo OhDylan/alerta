2c830bfe1cb0f498f3afd2476e966da63188b600
Dylan Oh <dylanoh_1128@outlook.com>
2023-09-27 22:09:04 +0800

    Added sample code for aws sdk and ably integration

--- ably_stream/index.ts
+++ ably_stream/index.ts
@@ -1,5 +1,104 @@
 import * as Ably from 'ably';
+import * as AWS from 'aws-sdk';
 
-let options: Ably.Types.ClientOptions = { key: 'foo' };
-let client = new Ably.Realtime(options); /* inferred type Ably.Realtime */
-let channel = client.channels.get('feed'); /* inferred type Ably.Types.RealtimeChannel */
+// Initialize the Ably client with API key
+const ably = new Ably.Realtime('API_KEY');
+
+// Initialize the AWS client
+AWS.config.update({
+  accessKeyId: 'ACCESS_KEY_ID',
+  secretAccessKey: 'SECRET_ACCESS_KEY',
+  region: 'REGION'
+});
+
+// Create an instance of the AWS fraud detector service
+const fraudDetector = new AWS.FraudDetector();
+
+// Define the Ably channel name and the AWS fraud detector model name
+const channelName = 'transactions';
+const modelName = 'fraud_detector_model';
+
+// Publish some transaction data to the Ably channel using the REST API
+const publishTransaction = async (transaction) => {
+  try {
+    // Create an instance of the Ably REST client
+    const rest = new Ably.Rest('YOUR_API_KEY');
+    // Get the channel object
+    const channel = rest.channels.get(channelName);
+    // Publish the transaction data to the channel
+    await channel.publish('transaction', transaction);
+    console.log('Published transaction:', transaction);
+  } catch (error) {
+    console.error('Error publishing transaction:', error);
+  }
+};
+
+// Subscribe to the Ably channel using the Realtime API and send the data to the AWS fraud detector
+const subscribeAndDetectFraud = () => {
+  try {
+    // Get the channel object
+    const channel = ably.channels.get(channelName);
+    // Subscribe to the channel and receive the transaction data
+    channel.subscribe('transaction', (message) => {
+      console.log('Received transaction:', message.data);
+      // Send the transaction data to the AWS fraud detector
+      detectFraud(message.data);
+    });
+  } catch (error) {
+    console.error('Error subscribing to channel:', error);
+  }
+};
+
+// Send the transaction data to the AWS fraud detector and receive the fraud prediction
+const detectFraud = async (transaction) => {
+  try {
+    // Define the fraud detector request parameters
+    const params = {
+      detectorId: modelName, // The name of the fraud detector model
+      eventId: transaction.eventId, // A unique identifier for the event
+      eventTypeName: 'online_transaction', // The event type name
+      eventTimestamp: transaction.eventTimestamp, // The event timestamp in ISO 8601 format
+      entities: [
+        {
+          entityType: 'customer', // The entity type
+          entityId: transaction.customerId // The entity identifier
+        }
+      ],
+      eventVariables: {
+        // The event variables
+        ip_address: transaction.ipAddress,
+        email_address: transaction.emailAddress,
+        billing_address: transaction.billingAddress,
+        shipping_address: transaction.shippingAddress,
+        payment_method: transaction.paymentMethod,
+        card_bin: transaction.cardBin,
+        order_amount: transaction.orderAmount
+      }
+    };
+    // Call the fraud detector service and get the fraud prediction
+    const response = await fraudDetector.getEventPrediction(params).promise();
+    console.log('Fraud prediction:', response);
+  } catch (error) {
+    console.error('Error detecting fraud:', error);
+  }
+};
+
+const sampleTransaction = {
+  eventId: '123456789',
+  eventTimestamp: '2023-09-27T13:45:17Z',
+  customerId: 'C001',
+  ipAddress: '192.168.0.1',
+  emailAddress: 'customer@example.com',
+  billingAddress: '123 Main Street, Singapore',
+  shippingAddress: '123 Main Street, Singapore',
+  paymentMethod: 'credit_card',
+  cardBin: '411111',
+  orderAmount: 100
+};
+
+// Publish the sample transaction data to the Ably channel
+publishTransaction(sampleTransaction);
+
+// Subscribe to the Ably channel and send the data to the AWS fraud detector
+subscribeAndDetectFraud();
+
